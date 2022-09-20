56729e9aec24e1665b4052cb81ac08e18cc77787
seba-razo <sebarazo14@gmail.com>
2022-09-20 01:26:01 -0300

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: ()=> import('./public/pages/login/login.module').then(m => m.LoginModule),
+  
+  },
+  {
+    path: 'contactos',
+    loadChildren: ()=> import('./public/pages/contactos/contactos.module').then(m => m.ContactosModule)
+  },
+  {
+    path: 'detalles-contactos',
+    loadChildren: ()=> import('./public/pages/detalles-contactos/detalles-contactos.module').then(m => m.DetallesContactosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/public/pages/detalles-contactos/detalles-contactos-routing.module.ts
+++ src/app/public/pages/detalles-contactos/detalles-contactos-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetallesContactosComponent } from './detalles-contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: DetallesContactosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
