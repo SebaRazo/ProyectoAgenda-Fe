136df8a7097cf29bc0c475dbf050eb9cf86ef11f
Romanx91 <romanpy7@gmail.com>
2022-09-26 15:16:47 -0300

    correccion de pahts

--- src/app/public/pages/agregar-contactos/agregar-contactos-routing.module.ts
+++ src/app/public/pages/agregar-contactos/agregar-contactos-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarContactosComponent } from './agregar-contactos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",
+  component: AgregarContactosComponent
+
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
