ae7a1acbb94226bd9633236360b4bb573c233ce3
seba-razo <sebarazo14@gmail.com>
2022-10-11 19:29:54 -0300

    Arreglos componentes y servicios

--- src/app/public/pages/login/login.component.ts
+++ src/app/public/pages/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { iAuthRequest } from 'src/app/core/interfaces/auth';
+import { AuthService } from 'src/app/core/services/auth.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+  export class LoginComponent {
 
-  ngOnInit(): void {
+    constructor(private auth:AuthService, private router:Router) { }
+  
+    //Hecho usando NgModel
+    authData:iAuthRequest = {
+      userName : "",
+      password : ""
+    };
+  
+  
+    async login(form:NgForm){
+      //Valor del formulario para no usar NgModel
+      console.log(form.value);
+      const token = await this.auth.login(form.value);
+      if(token) this.router.navigate(['']);
+    }
+  
   }
 
-}
