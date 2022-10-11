5cf308a3b2bf2134ddeefd179dacd4e15467b1ee
seba-razo <sebarazo14@gmail.com>
2022-10-11 19:31:07 -0300

    Arreglos componentes y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BottonLoginComponent } from './public/components/botton-login/botton-login.component';
-import { BottonRegistroComponent } from './public/components/botton-registro/botton-registro.component';
-import { BottonAgregarContactoComponent } from './public/components/botton-agregar-contacto/botton-agregar-contacto.component';
-import { AgregarListaNegraComponent } from './public/components/agregar-lista-negra/agregar-lista-negra.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BottonLoginComponent,
-    BottonRegistroComponent,
-    BottonAgregarContactoComponent,
-    AgregarListaNegraComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
